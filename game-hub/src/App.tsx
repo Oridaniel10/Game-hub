6d558d41a4e23121539a99c867556786825ad6c9
oridaniel10 <orizxzx@gmail.com>
2024-05-13 16:38:14 +0300

    Build Responsive Layout by grids

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: '"nav" "main"', // most devices
+        lg: '"nav nav" "aside main"', // large devices more the 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        {/* Showing the Aside grid only on large devices more than 1024px*/}
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
-
 export default App;

8ddf4436bc6f9b78d50025184aa66cbe121b11a5
oridaniel10 <orizxzx@gmail.com>
2024-05-18 15:32:07 +0300

    Fetch the Genres of games from Api

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
       </GridItem>
       <Show above="lg">
         {/* Showing the Aside grid only on large devices more than 1024px*/}
-        <GridItem area="aside">Aside</GridItem>
+        <GridItem area="aside">
+          <GenreList></GenreList>
+        </GridItem>
       </Show>
       <GridItem area="main">
         <GameGrid></GameGrid>
